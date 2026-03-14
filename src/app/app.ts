b5502c7c07fe3df1855fe028a111e7aaff39fa91
terminedev <terminegastondev@gmail.com>
2026-03-14 11:11:21 -0300

    Inputs del Chat Implementados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ChatView } from "./components/chat-view/chat-view";
 import { LayoutChat } from "./components/layout-chat/layout-chat";
+import { InputChat } from "./components/input-chat/input-chat";
 
 @Component({
   selector: 'app-root',
-  imports: [ChatView, LayoutChat],
+  imports: [ChatView, LayoutChat, InputChat],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/input-chat/input-chat.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input-chat',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './input-chat.html',
+  styleUrls: ['./input-chat.css']
+})
+export class InputChat {
+  messageControl = new FormControl('', [Validators.required, Validators.minLength(1)]);
+
+  showSuggestions = false;
+
+  suggestedMessages: string[] = [
+    '¿Cómo estás?',
+    '¿Qué ocurrió ayer?',
+    'Cuéntame más sobre eso.',
+    '¡Entendido, gracias!'
+  ];
+
+  toggleSuggestions() {
+    this.showSuggestions = !this.showSuggestions;
+  }
+
+  selectSuggestion(msg: string) {
+    this.messageControl.setValue(msg);
+    this.showSuggestions = false;
+  }
+
+  onSend() {
+    if (this.messageControl.valid) {
+      this.messageControl.reset('');
+    }
+  }
+
+  clearMessage() {
+    this.messageControl.reset('');
+  }
+}
