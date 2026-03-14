3f7a4fc2c21193d6ed175ad921a0aaa8fa56cd9a
terminedev <terminegastondev@gmail.com>
2026-03-14 11:00:54 -0300

    Layout del Chat implementado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { ChatView } from "./components/chat-view/chat-view";
+import { LayoutChat } from "./components/layout-chat/layout-chat";
 
 @Component({
   selector: 'app-root',
-  imports: [ChatView],
+  imports: [ChatView, LayoutChat],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/layout-chat/layout-chat.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-chat',
+  imports: [],
+  templateUrl: './layout-chat.html',
+  styleUrl: './layout-chat.css',
+})
+export class LayoutChat {
+
+}
